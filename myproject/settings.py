95d05c9845a0cd3a9749bce0059a34ecb7bc7ab6
İlayda Zehra Yılmaz <zehra.yilmaz@ug.bilkent.edu.tr>
2024-12-15 01:25:26 +0300

    ui push

--- myproject/settings.py
+++ myproject/settings.py
@@ -43,7 +43,14 @@ INSTALLED_APPS = [
     "myapp",
     'patient_admission',
     'inpatient',
+    "crispy_forms",
+    "crispy_bootstrap5",
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
